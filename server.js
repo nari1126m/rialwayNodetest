33f74a214596172fbc4c865cd6a9c8b37ffd858c
naro1126m <123956241+naro1126m@users.noreply.github.com>
2025-02-19 20:05:35 +0700

    updata_b_f-end

--- server.js
+++ server.js
@@ -2,14 +2,24 @@
 const path = require('path');
 const express = require('express');
 const schedule = require('node-schedule');
-
 const app = express();
 let lastdata = null;
-schedule.scheduleJob('*/10 * * * * *',async () => {
+let collect = [];
+schedule.scheduleJob('*/10 * * * *',async () => {
+    let i = 1;
+
+    collect = [];
 try{
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    while(true){
+        if(i > 200){
+            break;
+        }
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${i}`);
     lastdata = await response.json(); // Update the latest quote
     console.log('data :', lastdata);
+    collect.push(lastdata);
+    i = i + 1;
+    }
 }catch(error){
     console.error('fetch error : ',error)
 }
@@ -20,8 +30,8 @@ app.get('/',(req,res) => {
 });
 
 app.get('/api/data',(req,res) => {
-    if(!(lastdata === null)){
-        res.json(lastdata);
+    if(!(collect.length === 0)){
+        res.json(collect);
     }else{
         res.status(503).json('no data');
     }
